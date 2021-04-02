1ef5a74fa0dbf42966f5cd9aa67b28eebfadfddd
juniorlourenco <juniorminaj101@gmail.com>
2021-04-02 13:11:02 -0400

    integrating typescript, next and styled components

--- pages/src/components/Home/styles.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const Body = styled.View`
-    background: #747272;
-    display: flex;
-`;
---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Titulo</title>
+      </Head>
+      <main>
+        <h1>Hello world</h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
